fc2d08b7848fb60cb93a6b79ae5d15d77cfefe87
Mukai <k.mukai.work@gmail.com>
2025-09-01 16:13:19 +0900

    Add basic test script using Node's test runner

---
+++ test/basic.test.js
@@ -0,0 +1,8 @@
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+
+// Basic sample test to ensure test runner works
+
+test('adds numbers correctly', () => {
+  assert.equal(1 + 1, 2);
+});
