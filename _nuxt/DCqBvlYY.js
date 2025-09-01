30b0c4146960086dc2ec2620a3074b8d08eac0e3
mukai-work <mukai-work@users.noreply.github.com>
2025-09-01 03:48:28 +0000

    deploy: b1b3d5eed59a4555f0f49747f6f913c3af962ba8

--- _nuxt/DCqBvlYY.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DlAUqK2U.js";import{c as s,a as t,o as r}from"./Cq7mYpAJ.js";const a={},c={class:"p-10 text-center"};function n(d,e){return r(),s("div",c,e[0]||(e[0]=[t("h1",{class:"text-3xl font-bold"},"WeatherMood",-1),t("p",{class:"text-gray-600 mt-4"},"このページは仮置きです。",-1)]))}const f=o(a,[["render",n]]);export{f as default};
